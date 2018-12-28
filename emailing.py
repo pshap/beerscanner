9ca874e7b610e84c7111ab64ff46f39382569762
pshap <shapiro.peter@gmail.com>
2018-12-28 13:05:32 -0500

    emailing working, but still need to clean spec. characters

--- emailing.py
+++ emailing.py
@@ -1,24 +1,28 @@
 
 import smtplib
-try:
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-except:
-    print('Something went wrong...')
+from tabulate import tabulate
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import pandas as pd
 
 smtpserver = 'smtp.gmail.com:587'
 from_addr = 'beerscannernotif@gmail.com'
 to_addr_list = ['shapiro.peter@gmail.com']
-cc_addr_list = ['RC@xx.co.uk']
 subject = 'Timmys Beer Update'
-body = 'Howdy from a python function'
 login = 'beerscannernotif'
 password = 'beerscanner'
 
+newbeer = pd.read_csv('newbeer.csv')
+beerlist = pd.read_csv('beertable.csv')
+oldbeer = pd.read_csv('oldbeer.csv')
+beerlist = beerlist.set_index('Beer')
+
+#body = "Beerscanners Timmy OTooles Beer List Updates" + '\n' + newbeer
 
 header = ('Subject: %s' % subject)
-message = header + '\n' + body
+body = '\n' + 'New Beer' + tabulate(newbeer, headers='keys', tablefmt='psql') + '\n' + 'Beer List' + tabulate(beerlist, headers='keys', tablefmt='psql')+ '\n' + 'Kegs Kicked' + tabulate(oldbeer, headers='keys', tablefmt='psql')
 
+message = header + body
 
 server = smtplib.SMTP(smtpserver)
 server.starttls()
